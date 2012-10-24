b8a0490b80c1ad893f84515e31ecb6a71360e719
simpleavr <simpleavr@gmail.com>
2012-10-24 11:51:19 -0400

    2012 changes

--- zconv.pl
+++ zconv.pl
@@ -7,7 +7,7 @@ this is a perl script to convert the webbotlib speech sythesizer code from avr
 to msp430 usage. this script is use at your own risk giftware. webbotlib has
 it's own license terms.
 
-. locate "webbotlib" and download version 1.x, i tried w/ webbotavrclib-1.35
+. locate "webbotlib" and download version 2.x, i tried w/ webbotavrclib-1.35
 . inside the package locate and extract the Audio/Text2Speech directory
 . u will see the following files
 
@@ -28,6 +28,17 @@ vocab_en.c
   . include "webbot_speech.h"
   . say("Hello World");
   . enjoy :) and thanks webbot
+
+2012.10.24 change log
+
+. webbot_lib.h now reside in same directory
+. now uses webbotlib package 2.x versions
+. now compiles under CCS
+. adopt to new mspgcc
+  . mspgcc-20120406-p20120502
+  . use of #include <legacymsp430.h>
+  . retire own intrinsic __delay_cycle(), now uses mspgcc's
+  . adopt to newer interrupt naming convention
 
 <<< IMPORTANT >>>
 please observe the fact that webbotlib is GPL licensed and use / share accordingly
@@ -99,7 +110,7 @@ __END_soundOn
 
 #______ we are going to call the ouput package webbot_speech.h
 
-open O, ">../webbot_speech.h" or die;
+open O, "> webbot_speech.h" or die;
 
 #______ say() and speak() were the very old api (pre-webbotlib)
 print O <<__END_HEAD;
@@ -166,8 +177,17 @@ for my $ifile (qw/vocab_en.c sound2noise.c phoneme2sound.c speech2phoneme.c/) {
 		#____ need to place them in code segment, too big for ram
 		s/(static\s+)(uint8_t\s+SoundData\[\])/$1const $2/;
 
-		#____ init speech pause values
-		s/(static\s+uint8_t\s+timeFactor)/$1 = 16/;
+		#____ init speech pause values, not used pre 2.0 version
+		#s/(static\s+uint8_t\s+timeFactor)/$1 = 16/;
+		/_speech_timeFactor;\s*$/ and next;
+		s/=\s*_speech_timeFactor;/=16;/;
+
+		#____ works better w/ CCS
+		s/__inline__/__inline/;
+
+		#____ change delay time to constant
+		s/for\(r=16/for(r=delay/;
+		s/__delay_cycles\(delay\)/__delay_cycles(16)/;
 
 		#____ no need in msp430, it's automatic when we are "static const"
 		s/PROGMEM\s*//;
